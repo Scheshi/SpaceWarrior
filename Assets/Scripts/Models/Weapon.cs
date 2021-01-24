b9da8d9bf3443c5b7587c94786e5486b35388827
Artem Darari <enjerudar@gmail.com>
2021-01-24 03:46:16 -0800

    Hashing bullet pool in weapon

--- Assets/Scripts/Models/Weapon.cs
+++ Assets/Scripts/Models/Weapon.cs
@@ -11,11 +11,13 @@ namespace Asteroids
     {
         private Rigidbody2D _bullet;
         private Transform _startPositionTransform;
+        private BulletObjectPool _bulletPool;
         private Action _fireAction;
         private float _force;
         private float _damage;
         private const float _cooldown = 1.0f;
         private float _lastFireTime = 0.0f;
+        
 
         public Weapon(Rigidbody2D bullet, Transform startPositionTransform, ref Action fireAction, float force, float damage)
         {
@@ -24,6 +26,7 @@ namespace Asteroids
             _startPositionTransform = startPositionTransform;
             _force = force;
             _damage = damage;
+            _bulletPool = ServiceLocatorObjectPool.Get<BulletObjectPool>();
         }
 
         public void Fire()
@@ -31,7 +34,7 @@ namespace Asteroids
             if (_lastFireTime + _cooldown < Time.time)
             {
                 _lastFireTime = Time.time;
-                var bullet = ServiceLocatorObjectPool.Get<BulletObjectPool>().Get<Rigidbody2D>(_startPositionTransform.position, _damage);
+                var bullet = _bulletPool.Get<Rigidbody2D>(_startPositionTransform.position, _damage);
                 bullet.AddForce(_startPositionTransform.up * _force, ForceMode2D.Impulse);
             }
         }
